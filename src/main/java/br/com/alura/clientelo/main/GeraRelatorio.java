9e20b2a3eda7a0171c637c6716ffaffd94e42892
Leonardo Silva <leonardo.silva@caelum.com.br>
2022-11-01 13:00:26 -0300

    ajustes

--- src/main/java/br/com/alura/clientelo/main/GeraRelatorio.java
+++ src/main/java/br/com/alura/clientelo/main/GeraRelatorio.java
@@ -4,14 +4,12 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import br.com.alura.clientelo.ProcessadorDeCsv;
 import br.com.alura.clientelo.model.Pedido;
-import br.com.alura.clientelo.service.RelatorioService;
 
 public class GeraRelatorio {
 
@@ -23,8 +21,7 @@ public class GeraRelatorio {
 		
 		pedidos = ProcessadorDeCsv.processaArquivo("pedidos.csv");
 		
-		RelatorioService relatorioService = new RelatorioService(Optional.of(pedidos));
-		relatorioService.relatorioConsolidadoDePedidos();
+		
 	
 	}
 }
